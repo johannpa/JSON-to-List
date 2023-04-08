2df80eb6f3fecdf2b5682ce2fe1f025d1f5a1200
johannpa <johann_paque@hotmail.fr>
2023-04-08 22:25:58 +0200

    Create dbo.mytest.tblContact.sql

---
+++ dbo.mytest.tblContact.sql
@@ -0,0 +1,21 @@
+USE [MyTest]
+GO
+
+/****** Object:  Table [dbo].[Contact]    Script Date: 6/21/2022 12:57:25 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+ 
+CREATE TABLE [dbo].[Contact](
+	[ContactId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL, 
+	[Phone] [nvarchar](50) NULL,
+	[Email] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Contact] PRIMARY KEY CLUSTERED 
+(
+	[ContactId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
